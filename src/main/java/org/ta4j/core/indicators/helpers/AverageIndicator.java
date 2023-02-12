a3f4df8a16bd41f020e7d34440e074f2f008254d
Cypher01 <cypher@cypher.com>
2023-02-12 22:46:29 +0100

    Add average indicator

---
+++ src/main/java/org/ta4j/core/indicators/helpers/AverageIndicator.java
@@ -0,0 +1,29 @@
+package org.ta4j.core.indicators.helpers;
+
+import org.ta4j.core.Indicator;
+import org.ta4j.core.indicators.CachedIndicator;
+import org.ta4j.core.num.Num;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AverageIndicator extends CachedIndicator<Num> {
+	private final List<Indicator<Num>> indicators;
+
+	public AverageIndicator(Indicator<Num>... indicators) {
+		super(indicators[0]);
+
+		this.indicators = Arrays.asList(indicators);
+	}
+
+	@Override
+	protected Num calculate(int index) {
+		Num value = numOf(0);
+
+		for (Indicator<Num> indicator : indicators) {
+			value = value.plus(indicator.getValue(index));
+		}
+
+		return value.dividedBy(numOf(indicators.size()));
+	}
+}
