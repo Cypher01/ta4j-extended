8eaa5228627fdcc9c223b4ef4eaf0e27e370ea39
Cypher01 <cypher@cypher.com>
2023-03-06 23:19:58 +0100

    Add WaveTrend indicator

---
+++ src/main/java/org/ta4j/core/indicators/WaveTrendDIndicator.java
@@ -0,0 +1,23 @@
+package org.ta4j.core.indicators;
+
+import org.ta4j.core.Indicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * WaveTrend D indicator by LazyBear.
+ * <a href="https://www.tradingview.com/script/2KE8wTuF-Indicator-WaveTrend-Oscillator-WT/">TradingView</a>
+ */
+public class WaveTrendDIndicator extends AbstractIndicator<Num> {
+	private final Indicator<Num> indicator;
+
+	public WaveTrendDIndicator(WaveTrendKIndicator k) {
+		super(k.getBarSeries());
+
+		this.indicator = new SMAIndicator(k, 4);
+	}
+
+	@Override
+	public Num getValue(int index) {
+		return indicator.getValue(index);
+	}
+}
---
+++ src/main/java/org/ta4j/core/indicators/WaveTrendKIndicator.java
@@ -0,0 +1,38 @@
+package org.ta4j.core.indicators;
+
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.Indicator;
+import org.ta4j.core.indicators.helpers.CombineIndicator;
+import org.ta4j.core.indicators.helpers.NzIndicator;
+import org.ta4j.core.indicators.helpers.TransformIndicator;
+import org.ta4j.core.indicators.helpers.TypicalPriceIndicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * WaveTrend K indicator by LazyBear.
+ * <a href="https://www.tradingview.com/script/2KE8wTuF-Indicator-WaveTrend-Oscillator-WT/">TradingView</a>
+ */
+public class WaveTrendKIndicator extends AbstractIndicator<Num> {
+	private final Indicator<Num> indicator;
+
+	public WaveTrendKIndicator(BarSeries series) {
+		this(series, 10, 21);
+	}
+
+	public WaveTrendKIndicator(BarSeries series, int barCountChannel, int barCountAverage) {
+		super(series);
+
+		TypicalPriceIndicator ap = new TypicalPriceIndicator(series);
+		EMAIndicator esa = new EMAIndicator(ap, barCountChannel);
+		CombineIndicator apMinusEsa = CombineIndicator.minus(ap, esa);
+		EMAIndicator d = new EMAIndicator(TransformIndicator.abs(apMinusEsa), barCountChannel);
+		TransformIndicator divisor = TransformIndicator.multiply(d, 0.015);
+		NzIndicator ci = new NzIndicator(CombineIndicator.divide(apMinusEsa, divisor));
+		this.indicator = new EMAIndicator(ci, barCountAverage);
+	}
+
+	@Override
+	public Num getValue(int index) {
+		return indicator.getValue(index);
+	}
+}
