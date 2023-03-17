99a427f222a1039e9b7da84e6ec9ab65f4f55767
Cypher01 <cypher@cypher.com>
2023-03-17 22:49:57 +0100

    Add VIDYA indicator

---
+++ src/main/java/org/ta4j/core/indicators/VIDYAIndicator.java
@@ -0,0 +1,34 @@
+package org.ta4j.core.indicators;
+
+import org.ta4j.core.Indicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * Variable Index Dynamic Average (VIDYA) indicator by everget.
+ * <a href="https://de.tradingview.com/script/hdrf0fXV-Variable-Index-Dynamic-Average-VIDYA/">TradingView</a>
+ */
+public class VIDYAIndicator extends CachedIndicator<Num> {
+	private final Indicator<Num> indicator;
+	private final Indicator<Num> cmoIndicator;
+	private final Num alpha;
+
+	public VIDYAIndicator(Indicator<Num> indicator, int barCount) {
+		super(indicator);
+
+		this.indicator = indicator;
+		this.cmoIndicator = new CMOIndicator(indicator, barCount);
+		this.alpha = numOf(2d / (barCount + 1d));
+	}
+
+	@Override
+	protected Num calculate(int index) {
+		if (index == 0) {
+			return numOf(0);
+		}
+
+		Num cmoAbsNormValue = cmoIndicator.getValue(index).abs().dividedBy(numOf(100));
+		Num prevValue = getValue(index - 1);
+
+		return indicator.getValue(index).multipliedBy(alpha).multipliedBy(cmoAbsNormValue).plus(prevValue.multipliedBy(numOf(1).minus(alpha.multipliedBy(cmoAbsNormValue))));
+	}
+}
