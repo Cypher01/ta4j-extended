38636952f0c3d4e1fee0c5f77125b63f1d4aa8f2
Cypher01 <cypher@cypher.com>
2023-03-17 22:57:44 +0100

    Add hlcc4 price indicator

---
+++ src/main/java/org/ta4j/core/indicators/helpers/HLCC4PriceIndicator.java
@@ -0,0 +1,26 @@
+package org.ta4j.core.indicators.helpers;
+
+import org.ta4j.core.Bar;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.indicators.CachedIndicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * HLCC4 (high/low/close/close average) price indicator.
+ */
+public class HLCC4PriceIndicator extends CachedIndicator<Num> {
+	public HLCC4PriceIndicator(BarSeries series) {
+		super(series);
+	}
+
+	@Override
+	protected Num calculate(int index) {
+		Bar bar = getBarSeries().getBar(index);
+
+		Num highPrice = bar.getHighPrice();
+		Num lowPrice = bar.getLowPrice();
+		Num closePrice = bar.getClosePrice();
+
+		return highPrice.plus(lowPrice).plus(closePrice).plus(closePrice).dividedBy(numOf(4));
+	}
+}
