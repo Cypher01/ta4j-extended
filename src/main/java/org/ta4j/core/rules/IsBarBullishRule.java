dbfc0db9f88ad933253138420e327f0960d173ca
Cypher01 <cypher@cypher.com>
2023-03-29 01:19:09 +0200

    Add bar bullish/bearish rules

---
+++ src/main/java/org/ta4j/core/rules/IsBarBullishRule.java
@@ -0,0 +1,33 @@
+package org.ta4j.core.rules;
+
+import org.ta4j.core.Bar;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.Indicator;
+import org.ta4j.core.TradingRecord;
+import org.ta4j.core.indicators.AbstractIndicator;
+
+/**
+ * Rule to check if the bar for the given index is bullish.
+ * Its purpose is to support not only BarSeries but also an indicator of type Bar,
+ */
+public class IsBarBullishRule extends AbstractRule {
+	private final Indicator<Bar> indicator;
+
+	public IsBarBullishRule(BarSeries series) {
+		this(new AbstractIndicator<Bar>(series) {
+			@Override
+			public Bar getValue(int index) {
+				return getBarSeries().getBar(index);
+			}
+		});
+	}
+
+	public IsBarBullishRule(Indicator<Bar> indicator) {
+		this.indicator = indicator;
+	}
+
+	@Override
+	public boolean isSatisfied(int index, TradingRecord tradingRecord) {
+		return indicator.getValue(index).isBullish();
+	}
+}
