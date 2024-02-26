c0f301d0a32559b0126d6a8ce266164a443c46cd
Cypher01 <cypher@cypher.com>
2024-02-26 20:31:14 +0100

    Add BooleanCombineIndicator

---
+++ src/main/java/org/ta4j/core/indicators/helpers/BooleanCombineIndicator.java
@@ -0,0 +1,66 @@
+package org.ta4j.core.indicators.helpers;
+
+import org.ta4j.core.Indicator;
+import org.ta4j.core.indicators.CachedIndicator;
+import org.ta4j.core.indicators.helpers.BooleanTransformIndicator.BooleanTransformType;
+import org.ta4j.core.num.Num;
+
+/**
+ * Boolean combine indicator.
+ * This is based on BooleanTransformIndicator with the same logical operators,
+ * but instead of comparing one indicator to a coefficient, it compares two indicators.
+ * Additionally, the indicator supports converting true and false to 1 and 0.
+ */
+public class BooleanCombineIndicator extends CachedIndicator<Boolean> {
+	private final Indicator<Num> indicator1;
+	private final Indicator<Num> indicator2;
+	private final BooleanTransformType type;
+
+	public BooleanCombineIndicator(Indicator<Num> indicator1, Indicator<Num> indicator2, BooleanTransformType type) {
+		super(indicator1);
+
+		this.indicator1 = indicator1;
+		this.indicator2 = indicator2;
+		this.type = type;
+	}
+
+	@Override
+	protected Boolean calculate(int index) {
+		Num value1 = indicator1.getValue(index);
+		Num value2 = indicator2.getValue(index);
+
+		switch (type) {
+			case equals:
+				return value1.isEqual(value2);
+			case isGreaterThan:
+				return value1.isGreaterThan(value2);
+			case isGreaterThanOrEqual:
+				return value1.isGreaterThanOrEqual(value2);
+			case isLessThan:
+				return value1.isLessThan(value2);
+			case isLessThanOrEqual:
+				return value1.isLessThanOrEqual(value2);
+			default:
+				return false;
+		}
+	}
+
+	public BooleanToNumIndicator asNum() {
+		return new BooleanToNumIndicator(this);
+	}
+
+	public static class BooleanToNumIndicator extends CachedIndicator<Num> {
+		private final BooleanCombineIndicator indicator;
+
+		public BooleanToNumIndicator(BooleanCombineIndicator indicator) {
+			super(indicator);
+
+			this.indicator = indicator;
+		}
+
+		@Override
+		protected Num calculate(int index) {
+			return indicator.getValue(index) ? numOf(1) : numOf(0);
+		}
+	}
+}
