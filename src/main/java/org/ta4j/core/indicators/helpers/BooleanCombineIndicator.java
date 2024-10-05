eeb47487707f0f5339f268afc2bea3002d926037
Cypher01 <cypher@cypher.com>
2024-10-05 20:41:05 +0200

    Update to ta4j 0.17

--- src/main/java/org/ta4j/core/indicators/helpers/BooleanCombineIndicator.java
+++ src/main/java/org/ta4j/core/indicators/helpers/BooleanCombineIndicator.java
@@ -2,47 +2,52 @@ package org.ta4j.core.indicators.helpers;
 
 import org.ta4j.core.Indicator;
 import org.ta4j.core.indicators.CachedIndicator;
-import org.ta4j.core.indicators.helpers.BooleanTransformIndicator.BooleanTransformType;
 import org.ta4j.core.num.Num;
+
+import java.util.function.BiFunction;
 
 /**
  * Boolean combine indicator.
  * This is based on BooleanTransformIndicator with the same logical operators,
- * but instead of comparing one indicator to a coefficient, it compares two indicators.
+ * but instead of comparing one indicator to a constant, it compares two indicators.
  * Additionally, the indicator supports converting true and false to 1 and 0.
  */
 public class BooleanCombineIndicator extends CachedIndicator<Boolean> {
 	private final Indicator<Num> indicator1;
 	private final Indicator<Num> indicator2;
-	private final BooleanTransformType type;
+	private final BiFunction<Num, Num, Boolean> transform;
 
-	public BooleanCombineIndicator(Indicator<Num> indicator1, Indicator<Num> indicator2, BooleanTransformType type) {
+	public static BooleanCombineIndicator isEqual(Indicator<Num> indicator1, Indicator<Num> indicator2) {
+		return new BooleanCombineIndicator(indicator1, indicator2, Num::isEqual);
+	}
+
+	public static BooleanCombineIndicator isGreaterThan(Indicator<Num> indicator1, Indicator<Num> indicator2) {
+		return new BooleanCombineIndicator(indicator1, indicator2, Num::isGreaterThan);
+	}
+
+	public static BooleanCombineIndicator isGreaterThanOrEqual(Indicator<Num> indicator1, Indicator<Num> indicator2) {
+		return new BooleanCombineIndicator(indicator1, indicator2, Num::isGreaterThanOrEqual);
+	}
+
+	public static BooleanCombineIndicator isLessThan(Indicator<Num> indicator1, Indicator<Num> indicator2) {
+		return new BooleanCombineIndicator(indicator1, indicator2, Num::isLessThan);
+	}
+
+	public static BooleanCombineIndicator isLessThanOrEqual(Indicator<Num> indicator1, Indicator<Num> indicator2) {
+		return new BooleanCombineIndicator(indicator1, indicator2, Num::isLessThanOrEqual);
+	}
+
+	public BooleanCombineIndicator(Indicator<Num> indicator1, Indicator<Num> indicator2, BiFunction<Num, Num, Boolean> transform) {
 		super(indicator1);
 
 		this.indicator1 = indicator1;
 		this.indicator2 = indicator2;
-		this.type = type;
+		this.transform = transform;
 	}
 
 	@Override
 	protected Boolean calculate(int index) {
-		Num value1 = indicator1.getValue(index);
-		Num value2 = indicator2.getValue(index);
-
-		switch (type) {
-			case equals:
-				return value1.isEqual(value2);
-			case isGreaterThan:
-				return value1.isGreaterThan(value2);
-			case isGreaterThanOrEqual:
-				return value1.isGreaterThanOrEqual(value2);
-			case isLessThan:
-				return value1.isLessThan(value2);
-			case isLessThanOrEqual:
-				return value1.isLessThanOrEqual(value2);
-			default:
-				return false;
-		}
+		return transform.apply(indicator1.getValue(index), indicator2.getValue(index));
 	}
 
 	@Override
