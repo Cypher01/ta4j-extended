7e3ec9f7ead9855171f5bb56138d2dde35d763c5
André Marthaler <am@marthaler-it.com>
2025-01-18 14:12:53 +0100

    - fix Trendilo Indicator

--- src/main/java/org/ta4j/core/indicators/TrendiloBandIndicator.java
+++ src/main/java/org/ta4j/core/indicators/TrendiloBandIndicator.java
@@ -2,6 +2,7 @@ package org.ta4j.core.indicators;
 
 import org.ta4j.core.Indicator;
 import org.ta4j.core.indicators.averages.SMAIndicator;
+import org.ta4j.core.indicators.helpers.NzIndicator;
 import org.ta4j.core.indicators.helpers.TransformIndicator;
 import org.ta4j.core.num.Num;
 
@@ -16,7 +17,7 @@ public class TrendiloBandIndicator extends AbstractIndicator<Num> {
     public TrendiloBandIndicator(TrendiloIndicator trendiloIndicator, double multiplier, int barCount) {
         super(trendiloIndicator.getBarSeries());
 
-        TransformIndicator avpchPow = TransformIndicator.pow(trendiloIndicator, 2);
+        TransformIndicator avpchPow = TransformIndicator.pow(new NzIndicator(trendiloIndicator), 2);
         SMAIndicator avpchPowSma = new SMAIndicator(avpchPow, barCount);
         this.rms = TransformIndicator.multiply(TransformIndicator.sqrt(avpchPowSma), multiplier);
         this.barCount = barCount;
