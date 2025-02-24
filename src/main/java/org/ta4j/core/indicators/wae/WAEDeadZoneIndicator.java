bfccf066557f6221454a55d4fe0f4f19d363c0eb
André Marthaler <am@marthaler-it.com>
2025-02-24 15:44:53 +0100

    - add tests for WAE Dead Zone Indicator

--- src/main/java/org/ta4j/core/indicators/wae/WAEDeadZoneIndicator.java
+++ src/main/java/org/ta4j/core/indicators/wae/WAEDeadZoneIndicator.java
@@ -20,17 +20,28 @@ public class WAEDeadZoneIndicator extends AbstractIndicator<Num> {
     }
 
     public WAEDeadZoneIndicator(BarSeries series, int barCount, double multiplier) {
+        this(series, new ATRIndicatorPlus(series, barCount), barCount, multiplier);
+    }
+
+    public WAEDeadZoneIndicator(BarSeries series, AbstractIndicator abstractIndicator) {
+        this(series, abstractIndicator, 100, 3.7);
+    }
+
+    public WAEDeadZoneIndicator(BarSeries series, AbstractIndicator abstractIndicator, int barCount,
+            double multiplier) {
         super(series);
 
-        this.deadZone = TransformIndicator.multiply(new ATRIndicatorPlus(series, barCount), multiplier);
+        this.deadZone = TransformIndicator.multiply(abstractIndicator, multiplier);
         this.barCount = barCount;
     }
 
-    @Override public Num getValue(int index) {
+    @Override
+    public Num getValue(int index) {
         return deadZone.getValue(index);
     }
 
-    @Override public int getCountOfUnstableBars() {
+    @Override
+    public int getCountOfUnstableBars() {
         return barCount;
     }
 }
