edea209b6f9da984e6b5cb21d93314d75b9482a1
Andre Marthaler <am@marthaler-it.com>
2026-03-18 17:11:19 +0100

    - add missing tests for G Channel Indicator - fix StackOverflowError if using more than e.g., 1.000 bars - add BTC/USDT 1-hour historical data as a CSV file in test resources

--- src/test/java/org/ta4j/core/TestdataReader.java
+++ src/test/java/org/ta4j/core/TestdataReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
 import java.time.Instant;
 import org.ta4j.core.mocks.MockBarSeriesBuilder;
 import org.ta4j.core.num.NumFactory;
@@ -17,8 +18,29 @@ public class TestdataReader {
     }
 
     public BarSeries readCsv(String filename) throws IOException {
-        BarSeries series = new MockBarSeriesBuilder().withNumFactory(numFactory).build();
+        String durationFromFilename = filename.split("-")[1].split("\\.")[0];
+        Duration duration;
+        if (durationFromFilename.equals("1h")) {
+            duration = Duration.ofHours(1);
+        } else if (durationFromFilename.equals("1D")) {
+            duration = Duration.ofDays(1);
+        } else {
+            throw new IllegalArgumentException("Duration cannot be determined.");
+        }
+
         File file = new File("src/test/resources/" + filename);
+
+        return readCsv(file, duration);
+    }
+
+    public BarSeries readCsv(String filename, Duration duration) throws IOException {
+        File file = new File("src/test/resources/" + filename);
+
+        return readCsv(file, duration);
+    }
+
+    public BarSeries readCsv(File file, Duration duration) throws IOException {
+        BarSeries series = new MockBarSeriesBuilder().withNumFactory(numFactory).build();
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
@@ -39,8 +61,16 @@ public class TestdataReader {
                 double close = Double.parseDouble(values[7]);
                 double volume = Double.parseDouble(values[8]);
 
-                series.barBuilder().beginTime(beginTime).endTime(endTime).openPrice(open).closePrice(close).highPrice(
-                        high).lowPrice(low).volume(volume).add();
+                series.barBuilder()
+                        .timePeriod(duration)
+                        .beginTime(beginTime)
+                        .endTime(endTime)
+                        .openPrice(open)
+                        .closePrice(close)
+                        .highPrice(high)
+                        .lowPrice(low)
+                        .volume(volume)
+                        .add();
             }
 
         }
