b4f4048c1b3543610627e535a279815bbd42989e
Cypher01 <cypher@cypher.com>
2026-04-09 20:59:09 +0200

    Fix merge conflict, formatting

--- src/test/java/org/ta4j/core/TestdataReader.java
+++ src/test/java/org/ta4j/core/TestdataReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
 import java.time.Instant;
 import org.ta4j.core.mocks.MockBarSeriesBuilder;
 import org.ta4j.core.num.NumFactory;
